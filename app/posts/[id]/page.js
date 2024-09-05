6ca0e663caeffb06763632bb2f4674a8bcd1e5f8
Md.Rabiul Hasan <70057377+md-rabiul-hasan@users.noreply.github.com>
2024-09-05 17:47:57 +0600

    dynamic meta data

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,6 +1,14 @@
 import SinglePost from '@/lib/SinglePost';
 import React from 'react'
 
+export async function generateMetadata({ params }) {
+    const {id} = params;
+    const post = await SinglePost(id);
+    return {
+      title: post.title,
+    }
+  }
+
 export default async function PostDetails({params}) {
     const {id} = params;
     const post = await SinglePost(id);
